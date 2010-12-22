0a3495bacb7096d705000b8e65890a3b675ae146
Felix Rabe <public@felixrabe.net>
2010-12-22 02:53:55 +0100

    Implement fundamental data operations

--- README.txt
+++ README.txt
@@ -1,4 +1,27 @@
 Gentle Technology Preview (da92de4118f6fa91)
+============================================
+
+This Technology Preview implements the fundamental operations needed to store
+and retrieve data on a computer, using two simple dictionary data structures.
+
+One dictionary, called the content database, is a content-addressable store.
+The value is the data content, and the key is the SHA1 sum of that value.  By
+their nature, the key-value entries in the content database are immutable.
+
+The second dictionary, called the pointer database, stores references to these
+SHA1 keys.  The value is the SHA1 sum, and the key is a generated 128-bit
+random number.  The value is mutable.
+
+For the bootstrapping mechanism that surrounds the data store, generated random
+64-bit numbers are used as the definitive keys, if for no other reason than to
+help to stop thinking in terms of (more often than not) poorly-chosen
+identifiers.  So the bootstrap process is actually twofold, with both a
+technical as well as a philosophical component.
+
+
+License: LGPL 2.1+
+------------------
+
 Copyright (C) 2010  Felix Rabe
 
 This library is free software; you can redistribute it and/or
@@ -14,3 +37,19 @@ Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+Future features
+---------------
+
+* Smallest possible bootstrapping
+    - Move most Python code into the Gentle data store
+* Version control and global undo
+* Indexed meta data (maybe employing map/reduce)
+* Interfaces to legacy software:
+    - virtual file system
+    - mail server (SMTP + IMAP/POP3)
+    - HTTP
+    - command line
+    - programming language APIs
+
--- gentle_tech_preview_da92de4118f6fa91.py
+++ gentle_tech_preview_da92de4118f6fa91.py
@@ -22,25 +22,117 @@ Gentle Technology Preview (da92de4118f6fa91)
 
 me = "da92de4118f6fa91"
 
+from hashlib import sha1
 import os
+
 
 class FullyQualified(object):
 
+    USER_HOME = os.path.expanduser("~")
+    PARENT_DIR_1683b96af50b3bd9 = os.path.join(USER_HOME, ".gentle_data_1683b96af50b3bd9")
+    CONTENT_DB_DIR_6ebc8ee8333c1f4c = os.path.join(PARENT_DIR_1683b96af50b3bd9, "content_db_6ebc8ee8333c1f4c")
+    POINTER_DB_DIR_8b3675814942c0fe = os.path.join(PARENT_DIR_1683b96af50b3bd9, "pointer_db_8b3675814942c0fe")
+    
+    # Make sure those above directories exist
+    for directory in (PARENT_DIR_1683b96af50b3bd9, CONTENT_DB_DIR_6ebc8ee8333c1f4c, POINTER_DB_DIR_8b3675814942c0fe):
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
+    ## METHODS FOR GENTLE BOOTSTRAPPING
+
     @staticmethod
-    def generate_name_b0a08d9073e10594():
+    def generate_identifier_b0a08d9073e10594():
+        """
+        Generates a new 64-bit bootstrap identifier.
+        """
         name_length = 64 / 8  # 64 bits, 8 bytes
         new_name = os.urandom(name_length)
         return new_name.encode("hex")  # 8 -> 16 bytes
 
     @staticmethod
-    def resolve_9b97d1198d8da30d(name):
+    def resolve_identifier_9b97d1198d8da30d(name):
+        """
+        Resolves a 64-bit bootstrap identifier to a Python object.
+        """
         if name == me:
-            import gentle_tech_preview_da92de4118f6fa91
-            return gentle_tech_preview_da92de4118f6fa91
+            return g
         for member_name in dir(FullyQualified):
             if member_name.endswith("_" + name):
                 return getattr(FullyQualified, member_name)
         return None
 
-gen = FullyQualified.generate_name_b0a08d9073e10594
-ntl = FullyQualified.resolve_9b97d1198d8da30d
+    ## FUNDAMENTAL OPERATIONS IN A GENTLE SYSTEM
+    
+    @staticmethod
+    def sha1_42f2ba4c350bc32a(byte_string):
+        sha1_object = sha1()
+        sha1_object.update(byte_string)
+        return sha1_object.hexdigest()
+    
+    @staticmethod
+    def store_data_b1b4129b91991705(byte_string):
+        sha1fn = g.ntl("42f2ba4c350bc32a")
+        content_db_dir = g.ntl("6ebc8ee8333c1f4c")
+        
+        sha1key = sha1fn(byte_string)
+        filename = os.path.join(content_db_dir, sha1key)
+        file(filename, "wb").write(byte_string)
+        return sha1key
+    
+    @staticmethod
+    def remove_data_88eea22244f81249(sha1key):
+        content_db_dir = g.ntl("6ebc8ee8333c1f4c")
+        
+        filename = os.path.join(content_db_dir, sha1key)
+        os.remove(filename)
+   
+    @staticmethod
+    def get_data_056702875ab4e43b(sha1key):
+        content_db_dir = g.ntl("6ebc8ee8333c1f4c")
+        
+        filename = os.path.join(content_db_dir, sha1key)
+        byte_string = file(filename, "rb").read()
+        return byte_string
+    
+    @staticmethod
+    def generate_ptr_142e69d252889f3c():
+        key_length = 128 / 8  # 128 bits, 16 bytes
+        ptrkey = os.urandom(key_length)
+        return ptrkey.encode("hex")  # 16 -> 32 bytes
+    
+    @staticmethod
+    def store_ptr_2c98aaa88cfdc9fc(ptrkey, sha1key):
+        pointer_db_dir = g.ntl("8b3675814942c0fe")
+        
+        filename = os.path.join(pointer_db_dir, ptrkey)
+        file(filename, "wb").write(sha1key)
+    
+    @staticmethod
+    def remove_ptr_5518ac3c5c6eb04d(ptrkey):
+        pointer_db_dir = g.ntl("8b3675814942c0fe")
+        
+        filename = os.path.join(pointer_db_dir, ptrkey)
+        os.remove(filename)
+    
+    @staticmethod
+    def follow_ptr_711d9d3ba40a10ee(ptrkey):
+        pointer_db_dir = g.ntl("8b3675814942c0fe")
+        
+        filename = os.path.join(pointer_db_dir, ptrkey)
+        sha1key = file(filename, "rb").read()
+        return sha1key
+        
+
+import gentle_tech_preview_da92de4118f6fa91 as g
+FQ = FullyQualified
+gen = FullyQualified.generate_identifier_b0a08d9073e10594
+ntl = FullyQualified.resolve_identifier_9b97d1198d8da30d
+
+store_data = g.ntl("b1b4129b91991705")
+remove_data = g.ntl("88eea22244f81249")
+get_data = g.ntl("056702875ab4e43b")
+generate_ptr = g.ntl("142e69d252889f3c")
+store_ptr = g.ntl("2c98aaa88cfdc9fc")
+remove_ptr = g.ntl("5518ac3c5c6eb04d")
+follow_ptr = g.ntl("711d9d3ba40a10ee")
+
