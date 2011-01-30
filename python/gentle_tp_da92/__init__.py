9cd728a9e6cf3577afd5c2a79a1538587014a41f
Felix Rabe <public@felixrabe.net>
2011-01-31 00:07:20 +0100

    Add 'easy.Gentle' to 'from gentle_tp_da92 import *'

--- python/gentle_tp_da92/__init__.py
+++ python/gentle_tp_da92/__init__.py
@@ -1,22 +1,19 @@
 __gentle_tp_da92_id__ = \
     "da92de4118f6fa915b6bdd73f090ad57dc153082600855e5c7a85e8fe054c5a1"
 
-__all__ = [
-    # Core utilities:
-    "utilities",
+# Core utilities:
+from    . import utilities
 
-    # Data store interfaces and implementations:
-    "data_store_interfaces",
-    "fs_based",
-    "memory_based",
+# Data store interfaces and implementations:
+from    . import data_store_interfaces
+from    . import fs_based
+from    . import memory_based
 
-    # Gentle TP-DA92 Python API module:
-    "easy",
+# Gentle TP-DA92 Python API module:
+from    . import easy
 
-    # Command line interface, not provided:
-    # "__main__",
+# Utility modules for higher-level conventions:
+from    . import json
+from    . import time
 
-    # Utility modules for higher-level conventions:
-    "json",
-    "time",
-    ]
+Gentle = easy.Gentle
